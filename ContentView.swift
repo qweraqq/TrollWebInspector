e38ecf8ea6dd4be269dee4ffca8180ed42a6c3f7
xx <shenxiangxiang@gmail.com>
2025-12-01 21:41:23 +0800

    wip

--- ContentView.swift
+++ ContentView.swift
@@ -54,6 +54,9 @@ struct ContentView: View {
             do {
                 // 1. Locate Binaries
                 guard let injectorPath = Bundle.main.path(forResource: "injector", ofType: nil),
+                      let cpPath = Bundle.main.path(forResource: "cp", ofType: nil),
+                      let cp15Path = Bundle.main.path(forResource: "cp-15", ofType: nil),
+                      let chownPath = Bundle.main.path(forResource: "chown", ofType: nil),
                       let agentPath = Bundle.main.path(forResource: "agent", ofType: "dylib") else {
                     self.appendLog("[-] Error: 'injector' or 'agent.dylib' not found in Bundle.\n")
                     DispatchQueue.main.async { self.isBusy = false }
@@ -71,37 +74,61 @@ struct ContentView: View {
                 }
                 self.appendLog("[+] Found webinspectord at PID \(pid)\n")
                 
-                // 3. Construct Command
-                // injector -p <pid> -f agent.dylib -e entry_main
+
+                
+                
+                // Execute as Root
+                if #available(iOS 16, *) {
+                    let receipt = try Execute.rootSpawnWithOutputs(binary: cpPath, arguments: ["-f", agentPath, "/var/mobile/Library/Caches/xxyyxx.dylib"])
+                    self.appendLog(receipt.stdout)
+                    if !receipt.stderr.isEmpty {
+                        self.appendLog("[stderr] \(receipt.stderr)\n")
+                    }
+                } else {
+                    let receipt = try Execute.rootSpawnWithOutputs(binary: cp15Path, arguments: ["-f", agentPath, "/var/mobile/Library/Caches/xxyyxx.dylib"])
+                    self.appendLog(receipt.stdout)
+                    if !receipt.stderr.isEmpty {
+                        self.appendLog("[stderr] \(receipt.stderr)\n")
+                    }
+                    if case .exit(let code) = receipt.terminationReason {
+                        if code == 0 {
+                            self.appendLog("[+] CP Successful!\n")
+                        } else {
+                            self.appendLog("[-] CP exited with code \(code).\n")
+                        }
+                    }
+                }
+                // let retCode = try Execute.rootSpawn(binary: cp15Path, arguments: ["-f", agentPath, "/var/mobile/Library/Caches/xxyyxx.dylib"])
+                // self.appendLog("[-] CP exited with code \(retCode).\n")
+
+                // let chown_receipt = try Execute.rootSpawnWithOutputs(binary: chownPath, arguments: ["mobile:mobile", "/var/mobile/Library/Caches/xxyyxx.dylib"])
+                // self.appendLog(chown_receipt.stdout)
+                // if !chown_receipt.stderr.isEmpty {
+                //     self.appendLog("[stderr] \(chown_receipt.stderr)\n")
+                // }
+
+        
+                // Construct Command: injector -p <pid> -f agent.dylib -e entry_main
                 let args = [
                     "-p", "\(pid)",
-                    "-f", agentPath,
+                    "-f", "/var/mobile/Library/Caches/xxyyxx.dylib",
                     "-e", "entry_main"
                 ]
-                
                 self.appendLog("[*] Running: injector \(args.joined(separator: " "))\n")
-                let result = RootExecutor.run(binary: injectorPath, arguments: args)
-
-                if result == 0 {
-                    self.appendLog("injector successfully!")
-                } else {
-                    self.appendLog("Failed to injector. Exit code: \(result)")
+                let receipt = try Execute.rootSpawnWithOutputs(binary: injectorPath, arguments: args)
+                
+                self.appendLog(receipt.stdout)
+                if !receipt.stderr.isEmpty {
+                    self.appendLog("[stderr] \(receipt.stderr)\n")
                 }
-                // // 4. Execute as Root
-                // let receipt = try Execute.rootSpawnWithOutputs(binary: injectorPath, arguments: args)
                 
-                // self.appendLog(receipt.stdout)
-                // if !receipt.stderr.isEmpty {
-                //     self.appendLog("[stderr] \(receipt.stderr)\n")
-                // }
-                
-                // if case .exit(let code) = receipt.terminationReason {
-                //     if code == 0 {
-                //         self.appendLog("[+] Injection Successful!\n")
-                //     } else {
-                //         self.appendLog("[-] Injector exited with code \(code).\n")
-                //     }
-                // }
+                if case .exit(let code) = receipt.terminationReason {
+                    if code == 0 {
+                        self.appendLog("[+] Injection Successful!\n")
+                    } else {
+                        self.appendLog("[-] Injector exited with code \(code).\n")
+                    }
+                }
                 
             } catch {
                 self.appendLog("[-] Exception: \(error.localizedDescription)\n")
--- package-tipa.sh
+++ package-tipa.sh
@@ -7,20 +7,40 @@ mkdir Payload
 cp -r .theos/obj/TrollWebInspector.app Payload/
 cp Info.plist Payload/TrollWebInspector.app/Info.plist
 
-if [ -f "injector" ] && [ -f "agent.dylib" ]; then
+if [ -f "injector" ] && [ -f "agent.dylib" ] ; then
     echo "[*] Copying injector and agent.dylib..."
     cp injector Payload/TrollWebInspector.app/
     cp agent.dylib Payload/TrollWebInspector.app/
+
 else
     echo "[-] ERROR: 'injector' or 'agent.dylib' missing in project root!"
     exit 1
 fi
 
-cp -r .theos/obj/RootHelper Payload/TrollWebInspector.app/RootHelper
-chmod 755 Payload/TrollWebInspector.app/RootHelper
-./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/RootHelper
+cp cp Payload/TrollWebInspector.app/
+cp cp-15 Payload/TrollWebInspector.app/
+cp chown Payload/TrollWebInspector.app/
+cp libiosexec.1.dylib Payload/TrollWebInspector.app/
+cp libintl.8.dylib Payload/TrollWebInspector.app/
+cp libxar.1.dylib Payload/TrollWebInspector.app/
+cp libcrypto.3.dylib Payload/TrollWebInspector.app/
+
 chmod 755 Payload/TrollWebInspector.app/TrollWebInspector
 chmod 755 Payload/TrollWebInspector.app/injector
+chmod 755 Payload/TrollWebInspector.app/cp
+chmod 755 Payload/TrollWebInspector.app/cp-15
+chmod 755 Payload/TrollWebInspector.app/chown
+chmod 755 Payload/TrollWebInspector.app/libiosexec.1.dylib
+chmod 755 Payload/TrollWebInspector.app/agent.dylib
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/cp
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/cp-15
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/libiosexec.1.dylib
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/libintl.8.dylib
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/libxar.1.dylib
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/libcrypto.3.dylib
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/chown
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/agent.dylib
+./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/injector
 ./ldid -STrollWebInspector.entitlements Payload/TrollWebInspector.app/TrollWebInspector
 
 zip -r TrollWebInspector.tipa Payload
